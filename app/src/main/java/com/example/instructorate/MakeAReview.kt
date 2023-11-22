5fc3ef478719762637662550fce8c2f20a7a4765
Adi <adi@macbook.lan>
2023-11-22 16:59:40 +0800

    New edits

--- app/src/main/java/com/example/instructorate/MakeAReview.kt
+++ app/src/main/java/com/example/instructorate/MakeAReview.kt
@@ -12,5 +12,13 @@ class MakeAReview : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.make_a_review)
+
+        val cancelBtn = findViewById<Button>(R.id.cancel_form_btn)
+        cancelBtn.setOnClickListener {
+            // TODO: add confirmation before proceeding with cancellation of form
+            val intent = Intent(this, StudentDashboard::class.java)
+            val msg = intent.putExtra("name", "Juan")
+            startActivity(intent)
+        }
     }
 }
